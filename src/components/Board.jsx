8befbae3ff340b192314ed821c7ad55a8736c0a0
Corey Long <hello@coreylong.dev>
2025-08-20 09:44:44 -0400

    styling looks good for now. taking break to get into logic

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,15 +2,15 @@ function Board() {
 
     return(
         <div className="board">
-            <div className="cell">1</div>
-            <div className="cell">2</div>
-            <div className="cell">3</div>
-            <div className="cell">4</div>
-            <div className="cell">5</div>
-            <div className="cell">6</div>
-            <div className="cell">7</div>
-            <div className="cell">8</div>
-            <div className="cell">9</div>
+            <div className="cells" id="cellOne">1</div>
+            <div className="cells" id="cellTwo">2</div>
+            <div className="cells" id="cellThree">3</div>
+            <div className="cells" id="cellFour">4</div>
+            <div className="cells" id="cellFive">5</div>
+            <div className="cells" id="cellSix">6</div>
+            <div className="cells" id="cellSeven">7</div>
+            <div className="cells" id="cellEight">8</div>
+            <div className="cells" id="cellNine">9</div>
         </div>
     )
 }
